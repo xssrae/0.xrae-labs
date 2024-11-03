bdf8e5ceef18692b20db9d2041c42dbc1f99e926
Rae <raquel.fontes28@icloud.com>
2024-11-03 19:50:23 -0300

    fix: image icon

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "Rae Labs",
-    image: "/avatar.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/icon.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "IT engineer",
     bio: "Cybersecurity, Mobile Security, Application Security.",
     email: "fontesraquel@proton.me",
