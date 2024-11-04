a29ce1fa4c4c96d12f2d5cce8f0d860d1ba86014
Rae <raquel.fontes28@icloud.com>
2024-11-03 21:35:22 -0300

    update blog icon

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "Rae Labs",
-    image: "/icon.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/icon.jpg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "IT engineer",
     bio: "Cybersecurity, Mobile Security, Application Security.",
     email: "fontesraquel@proton.me",
